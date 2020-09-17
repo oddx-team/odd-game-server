23eec4170557acd2e66b1c086bb75f9fbb4ee385
Huynh Minh Tu <tu.huynh@linecorp.com>
2020-09-17 21:00:41 +0700

    Split gen card func (#1)
    * Split gen card func
    
    * Make it beautiful

--- src/modules/card/cardService.ts
+++ src/modules/card/cardService.ts
@@ -2,12 +2,20 @@ import { Card, CARD_COLLECTION } from './cardModel'
 
 export interface CardService {
   getCards: (language: string) => Promise<Card[]>
+  generateCards: (color: string, size: number) => Promise<Card[]>
 }
 
 const cardService: CardService = {
   async getCards (language) {
     return await global.mongoDB.collection(CARD_COLLECTION)
       .find({ language }).sort({ $nature: -1 }).toArray() as Card[]
+  },
+  async generateCards (color, size) {
+    const cardCollection = global.mongoDB.collection(CARD_COLLECTION)
+    return await cardCollection.aggregate([
+      { $match: { color } },
+      { $sample: { size } }
+    ]).toArray() as Card[]
   }
 }
 
--- src/modules/room/roomService.ts
+++ src/modules/room/roomService.ts
@@ -1,6 +1,6 @@
 import { Room, RoomStatus, RoomChat, ROOM_CHAT_COLLECTION, ROOM_COLLECTION } from 'modules/room/roomModel'
 import { paramCase } from 'change-case'
-import { Card, CARD_COLLECTION } from 'modules/card/cardModel'
+import cardService from 'modules/card/cardService'
 
 export interface RoomService {
   createRoom: (country: string, host: string, name: string, size: number) => Promise<RoomCreateResult>
@@ -54,17 +54,10 @@ const roomService: RoomService = {
       .find({ country }).sort({ $nature: -1 }).toArray() as Room[]
   },
   async getRoom (slug) {
-    const cardCollection = global.mongoDB.collection(CARD_COLLECTION)
-    const blackCard = await cardCollection.aggregate([
-      { $match: { color: 'black' } },
-      { $sample: { size: 1 } }
-    ]).toArray() as Card[]
-    const cards = await cardCollection.aggregate([
-      { $match: { color: 'white' } },
-      { $sample: { size: 10 } }
-    ]).toArray() as Card[]
-    const blackCardId = blackCard[0]._id
-    const collectionCardIds = cards.map(card => card._id)
+    const blackCard = await cardService.generateCards('black', 1)
+    const whiteCards = await cardService.generateCards('white', 10)
+    const blackCardId = blackCard.pop()?._id
+    const collectionCardIds = whiteCards.map(card => card._id)
 
     return {
       roomInfo: {
